2af120310abaef9d735bf99d544fba7fd8922f96
Alex <ssaasshha11@yandex.ru>
2022-09-17 22:16:23 +0400

    Add new SnakeException

---
+++ src/Snake.Core/Exceptions/RewardObjectCantBeInWallPosException.cs
@@ -0,0 +1,9 @@
+﻿namespace Snake.Core.Exceptions
+{
+    public class RewardObjectCantBeInWallPosException : SnakeException
+    {
+        public RewardObjectCantBeInWallPosException() : base("Invalid position for reward object.")
+        {
+        }
+    }
+}
