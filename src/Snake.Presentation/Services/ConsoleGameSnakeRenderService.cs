de3e9d70def0cd6dc7a3b72cb00658cf59707a50
Alex <ssaasshha11@yandex.ru>
2022-09-17 22:17:58 +0400

    Add render service

---
+++ src/Snake.Presentation/Services/ConsoleGameSnakeRenderService.cs
@@ -0,0 +1,42 @@
+﻿using Snake.Application.Models;
+using Snake.Core.ValueObjects;
+using System.Xml.Linq;
+
+namespace Snake.Presentation.Services
+{
+    public class ConsoleGameSnakeRenderService : IGameSnakeRenderService
+    { 
+        public void RenderRewardObjects(IEnumerable<RewardObject> rewards)
+        {
+            rewards.ToList().ForEach(i =>
+            {
+                Console.SetCursorPosition(i.Position.X, i.Position.Y);
+                Console.WriteLine('*');
+            });
+        }
+
+        public void RenderSnakeBodyObject(SnakeBodyObject snake)
+        {
+            var allBody = new List<PosXY>();
+            allBody.AddRange(snake.Body);
+            allBody.Add(snake.Head);
+            allBody.Add(snake.Tail);
+            allBody.ForEach(i =>
+            {
+                if (i == null)
+                    return;
+                Console.SetCursorPosition(i.X, i.Y);
+                Console.WriteLine('\u2500');
+            });
+        }
+
+        public void RenderWalls(IEnumerable<PosXY> walls)
+        {
+            walls.ToList().ForEach(i =>
+            {
+                Console.SetCursorPosition(i.X, i.Y);
+                Console.WriteLine('\u2593');
+            });
+        }
+    }
+}
---
+++ src/Snake.Presentation/Services/IGameSnakeRenderService.cs
@@ -0,0 +1,12 @@
+﻿using Snake.Application.Models;
+using Snake.Core.ValueObjects;
+
+namespace Snake.Presentation.Services
+{
+    public interface IGameSnakeRenderService
+    {
+        public void RenderSnakeBodyObject(SnakeBodyObject snakeBody);
+        public void RenderWalls(IEnumerable<PosXY> walls);
+        public void RenderRewardObjects(IEnumerable<RewardObject> rewards);
+    }
+}
