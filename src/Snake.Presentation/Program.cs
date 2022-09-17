2af3d1d9b261cbca0e77f8f151c9357d639eba12
Alex <ssaasshha11@yandex.ru>
2022-09-17 22:19:19 +0400

    init game at console

--- src/Snake.Presentation/Program.cs
+++ src/Snake.Presentation/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.VisualBasic.FileIO;
+using Snake.Application.Adapters;
+using Snake.Core.Factories;
+using Snake.Core.ValueObjects;
+using Snake.Presentation;
+using Snake.Presentation.Services;
+
+ISnakeGameObjectFactory snakeGameObjectFactory = new SnakeGameObjectFactory();
+ISnakeGameManagerFactory gameManagerFactory = new SnakeGameManagerFactory();
+ISnakeGameService gameService = new SnakeGameService(gameManagerFactory, snakeGameObjectFactory);
+IGameSnakeRenderService render = new ConsoleGameSnakeRenderService();
+
+var level = new Level(new(20, 40), new List<PosXY>() { });
+var startPos = new PosXY(5, 5);
+var gameEngine = new GameEngine(gameService, render);
+gameEngine.Start(startPos, level);
