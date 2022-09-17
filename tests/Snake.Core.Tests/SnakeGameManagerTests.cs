5b3267deade146cc18ac24dcc8bedee21e9197d0
Alex <ssaasshha11@yandex.ru>
2022-09-17 22:19:56 +0400

    Add tests for Core project

---
+++ tests/Snake.Core.Tests/SnakeGameManagerTests.cs
@@ -0,0 +1,109 @@
+﻿using Shouldly;
+using Snake.Core.Domain;
+using Snake.Core.Exceptions;
+using Snake.Core.Factories;
+using Snake.Core.ValueObjects;
+
+namespace Snake.Core.Tests
+{
+    public class SnakeGameManagerTests
+    {
+        [Fact]
+        public void SnakeGameManager_Should_Throw_WrongDirectionException_When_Choose_Reverse_Direction()
+        {
+            // Arrange
+            var gameManager = GetGameManagerWithDeafaultParams();
+
+            // Act
+            gameManager.ChangeDirection(Constants.Direction.RIGHT);
+            var exception = Record.Exception(() => gameManager.ChangeDirection(Constants.Direction.LEFT));
+
+            // Assert
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<WrongDirectionException>();
+        }
+
+        [Fact]
+        public void Reward_Object_That_Added_At_Position_That_Contain_Other_Reward_Object_Will_Update_It()
+        {
+            // Arrange
+            var firstRewardObject = new RewardObject(new PosXY(1, 1), 4);
+            var secondRewardObject = new RewardObject(new PosXY(1, 1), 8);
+            var gameManager = GetGameManagerWithDeafaultParams();
+
+            // Act
+            gameManager.AddRewardObject(firstRewardObject);
+            gameManager.AddRewardObject(secondRewardObject);
+
+            // Assert
+            gameManager.GetRewardObjects().Count.ShouldBe(1);
+            gameManager.GetRewardObjects().First().Reward.ShouldBe((uint)8);
+        }
+        [Fact]
+        public void When_SnakeGameObject_Is_Dead_GameManager_GameOver_Should_Be_True()
+        {
+            // Arrange
+            var snake = GetSnakeInDefaultPosition();
+            var gameManager = snakeGameManagerFactory
+                .CreateSnakeGameManager(snake,
+                    new Level(new PosXY(10, 10),
+                        Enumerable.Empty<PosXY>()));
+
+            // Act
+            snake.KillSnake();
+
+            // Assert
+            gameManager.GameIsOver().ShouldBeTrue();
+        }
+
+        [Fact]
+        public void Game_Should_Be_Over_If_GameOver_Condition_For_Score_Will_Be_Added()
+        {
+            // Arrange
+            var gameManager = GetGameManagerWithDeafaultParams();
+
+            // Act
+            gameManager.AddGameOverConditions((gm) => gm.Score >= 5);
+            gameManager.AddScore(5);
+
+            // Assert
+            gameManager.GameIsOver().ShouldBeTrue();
+        }
+
+        [Fact]
+        public void If_Field_Is_End_Snake_Should_Appear_At_Reverse_End()
+        {
+            // Arrange
+            var snake = GetSnakeInDefaultPosition();
+            var gameManager = snakeGameManagerFactory
+                .CreateSnakeGameManager(snake,
+                    new Level(new PosXY(5, 10),
+                        Enumerable.Empty<PosXY>()));
+
+            // Act
+            for (int i = 0; i < 5; i++)
+            {
+                gameManager.MoveSnake();
+            }
+
+            // Assert
+            snake.GetHead().ShouldBe(new PosXY(0, 0));
+        }
+        #region ARRANGE
+        private readonly ISnakeGameObjectFactory snakeGameObjectFactory
+            = new SnakeGameObjectFactory();
+
+        private readonly ISnakeGameManagerFactory snakeGameManagerFactory
+            = new SnakeGameManagerFactory();
+
+        private SnakeGameObject GetSnakeInDefaultPosition()
+            => snakeGameObjectFactory.CreateSnakeGameObject(new(0, 0));
+
+        private SnakeGameManager GetGameManagerWithDeafaultParams()
+            => snakeGameManagerFactory
+                .CreateSnakeGameManager(GetSnakeInDefaultPosition(),
+                    new Level(new PosXY(10, 10),
+                            Enumerable.Empty<PosXY>()));
+        #endregion
+    }
+}
---
+++ tests/Snake.Core.Tests/SnakeGameObjectTests.cs
@@ -0,0 +1,94 @@
+﻿using Shouldly;
+using Snake.Core.Exceptions;
+using Snake.Core.Factories;
+using Snake.Core.ValueObjects;
+
+namespace Snake.Core.Tests
+{
+    public class SnakeGameObjectTests
+    {
+        [Fact]
+        public void SnakeLifeState_Change_Correctly()
+        {
+            // Arrange
+            var startPos = new PosXY(0, 0);
+            var snake = snakeGameObjectFactory.CreateSnakeGameObject(startPos);
+
+            // Act
+            snake.KillSnake();
+
+            // Assert
+            snake.SnakeIsDead().ShouldBeTrue();
+            snake.SnakeIsAlive().ShouldBeFalse();
+        }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(1, 2)]
+        public void Snake_Should_Collide_With_Object_At_Pos(int x, int y)
+        {
+            // Arrange
+            var startPos = new PosXY(x, y);
+            var snake = snakeGameObjectFactory.CreateSnakeGameObject(startPos);
+
+            // Act
+            var result = snake.CheckCollisionAtPosition(startPos);
+
+            // Assert
+            result.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void When_Snake_Is_Dead_Move_Will_Throw_TryMoveWhenDeadException()
+        {
+            // Arrange
+            var startPos = new PosXY(0, 0);
+            var snake = snakeGameObjectFactory.CreateSnakeGameObject(startPos);
+
+            // Act
+            snake.KillSnake();
+            var exception = Record.Exception(() => snake.Move(startPos with { X = 1 }));
+
+            // Assert
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<TryMoveWhenDeadException>();
+        }
+        [Theory]
+        [InlineData(1, 0)]
+        [InlineData(2, 1)]
+        [InlineData(3, 2)]
+        [InlineData(7, 6)]
+        public void Test_Body_Count_When_Increasing_Snake_With_Moving(int increaseCount, int bodySize)
+        {
+            // Arrange
+            var startPos = new PosXY(0, 0);
+            var snake = snakeGameObjectFactory.CreateSnakeGameObject(startPos);
+
+            // Act
+            for (int i = 0; i < increaseCount; i++)
+            {
+                snake.IncreaseSnake();
+                snake.Move(startPos with { X = i + 1});
+            }
+
+            // Assert
+            snake.GetBody().Count().ShouldBe(bodySize);
+        }
+        [Fact]
+        public void When_Snake_Was_Not_Increased_Tail_Is_Null()
+        {
+            // Arrange
+            var startPos = new PosXY(0, 0);
+            var snake = snakeGameObjectFactory.CreateSnakeGameObject(startPos);
+
+            // Act
+
+            // Assert
+            snake.GetTail().ShouldBeNull();
+        }
+        #region ARRANGE
+        private readonly ISnakeGameObjectFactory snakeGameObjectFactory
+            = new SnakeGameObjectFactory();
+        #endregion
+    }
+}
