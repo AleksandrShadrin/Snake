640273aac4abf6f32f0adf07b4d5b37171694e42
Alex <ssaasshha11@yandex.ru>
2022-09-18 17:43:07 +0400

    Add custom exceptions

---
+++ src/Snake.Application/Exceptions/InvalidJsonDeserializationException.cs
@@ -0,0 +1,15 @@
+﻿using Snake.Core.Exceptions;
+
+namespace Snake.Application.Exceptions
+{
+    public class InvalidJsonDeserializationException : SnakeException
+    {
+        public string FileName { get; private set; }
+
+        public InvalidJsonDeserializationException(string fileName) : base($"Data in {fileName} file is invalid.")
+        {
+            FileName = fileName;
+        }
+
+    }
+}
---
+++ src/Snake.Application/Exceptions/SaveFileDontExistException.cs
@@ -0,0 +1,13 @@
+﻿using Snake.Core.Exceptions;
+
+namespace Snake.Application.Exceptions
+{
+    public class SaveFileDontExistException : SnakeException
+    {
+        public string FileName { get; set; }
+        public SaveFileDontExistException(string fileName) : base($"File {fileName} don't exist.")
+        {
+            FileName = fileName;
+        }
+    }
+}
