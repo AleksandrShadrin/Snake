184f70179900a9e56fac361e92d882e7b89de8f9
Alex <ssaasshha11@yandex.ru>
2022-09-18 17:43:35 +0400

    Add data model for SaveLoad features

---
+++ src/Snake.Application/Models/SnakeGameData.cs
@@ -0,0 +1,7 @@
+﻿using Snake.Core.Constants;
+using Snake.Core.ValueObjects;
+
+namespace Snake.Application.Models
+{
+    public record SnakeGameData(SnakeBodyObject Snake, List<RewardObject> RewardObjects, Level Level, Direction Direction);
+}
