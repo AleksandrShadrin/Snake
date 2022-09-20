a27e21dd199234bfc4e0e076e57f96051e40dee6
Alex <ssaasshha11@yandex.ru>
2022-09-20 20:48:53 +0400

    Implement interface for simple case

---
+++ src/Snake.Presentation/LevelGenerator/DefaultLevelGenerator.cs
@@ -0,0 +1,10 @@
+﻿using Snake.Core.ValueObjects;
+
+namespace Snake.Presentation.LevelGenerator
+{
+    internal class DefaultLevelGenerator : ILevelGenerator
+    {
+        public (PosXY, Level) GenerateLevel()
+        => (new PosXY(0, 0), new Level(new PosXY(30, 30), Enumerable.Empty<PosXY>()));
+    }
+}
