f3dc2dd4b50c980bfe61bf9b2c23e5161530643e
Alex <ssaasshha11@yandex.ru>
2022-09-20 20:43:59 +0400

    Add custom exception

---
+++ src/Snake.Core/Exceptions/TryChangeSnakeDirectionMoreThenOncePerMoveException.cs
@@ -0,0 +1,9 @@
+﻿namespace Snake.Core.Exceptions
+{
+    public class TryChangeSnakeDirectionMoreThenOncePerMoveException : SnakeException
+    {
+        public TryChangeSnakeDirectionMoreThenOncePerMoveException() : base("Can't change direction more than one time per move.")
+        {
+        }
+    }
+}
