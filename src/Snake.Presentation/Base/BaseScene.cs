10a1ce704a95716fc83bd3c5eff54b1cf476972a
Alex <ssaasshha11@yandex.ru>
2022-09-20 20:48:03 +0400

    Add base classes for interfaces

---
+++ src/Snake.Presentation/Base/BaseScene.cs
@@ -0,0 +1,21 @@
+﻿using Snake.Presentation.Renderable;
+
+namespace Snake.Presentation.Base
+{
+    public abstract class BaseScene : BaseSelectable, IRenderable
+    {
+        public abstract void StartScene();
+        public abstract void DoOnKeyPressed();
+        public abstract void Render();
+        public Action<string> OnSwitchScene { get; set; }
+
+
+        public void OnKeyPressed()
+        {
+            if(Selected is false)
+                return;
+
+            DoOnKeyPressed();
+        }
+    }
+}
---
+++ src/Snake.Presentation/Base/BaseSelectable.cs
@@ -0,0 +1,19 @@
+﻿using Snake.Presentation.Selectable;
+
+namespace Snake.Presentation.Base
+{
+    public class BaseSelectable : ISelectable
+    {
+        public bool Selected { get; private set; } = false;
+
+        public void Select()
+        {
+            Selected = true;
+        }
+
+        public void UnSelect()
+        {
+            Selected = false;
+        }
+    }
+}
