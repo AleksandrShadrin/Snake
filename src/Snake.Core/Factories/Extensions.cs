8278b0fe30e16f06f816d2595af9736c9e26e43a
Alex <ssaasshha11@yandex.ru>
2022-09-20 20:44:24 +0400

    Add registration of factories

---
+++ src/Snake.Core/Factories/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Snake.Core.Factories
+{
+    public static class Extensions
+    {
+        public static IServiceCollection RegisterFactories(this IServiceCollection services)
+        {
+            services.AddSingleton<ISnakeGameManagerFactory, SnakeGameManagerFactory>();
+            services.AddSingleton<ISnakeGameObjectFactory, SnakeGameObjectFactory>();
+            return services;
+        }
+    }
+}
