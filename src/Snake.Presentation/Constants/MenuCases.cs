3b462a027b2197d9ab9bf39d4b56c349f2eb9343
Alex <ssaasshha11@yandex.ru>
2022-09-20 20:48:28 +0400

    Add constant class for menu options representation

---
+++ src/Snake.Presentation/Constants/MenuCases.cs
@@ -0,0 +1,11 @@
+﻿namespace Snake.Presentation.Constants
+{
+    public enum MenuCases
+    {
+        START_GAME = 1,
+        CONTINUE_GAME = 2,
+        LOAD_GAME = 3,
+        SAVE_GAME = 4,
+        EXIT = 5,
+    }
+}
