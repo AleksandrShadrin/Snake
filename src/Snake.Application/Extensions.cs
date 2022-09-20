0af425250733b37667dfb230c8be1ac9c5904e6b
Alex <ssaasshha11@yandex.ru>
2022-09-20 20:42:38 +0400

    Add service registration

---
+++ src/Snake.Application/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Snake.Application.Adapters;
+
+namespace Snake.Application
+{
+    public static class Extensions
+    {
+        public static IServiceCollection RegisterApplication(this IServiceCollection services)
+        {
+            services.AddSingleton<IGameSaveLoader, JsonGameSaveLoader>();
+            services.AddSingleton<ISnakeGameService, SnakeGameService>();
+            return services;
+        }
+    }
+}
