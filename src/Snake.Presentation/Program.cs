7264b77aa54f82ea42a5364fd7bf54c06cd16797
Alex <ssaasshha11@yandex.ru>
2022-09-20 20:50:27 +0400

    Rework project startup

--- src/Snake.Presentation/Program.cs
+++ src/Snake.Presentation/Program.cs
@@ -1,17 +1,34 @@
-﻿using Microsoft.VisualBasic.FileIO;
-using Snake.Application.Adapters;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Snake.Application;
 using Snake.Core.Factories;
-using Snake.Core.ValueObjects;
 using Snake.Presentation;
+using Snake.Presentation.Controller;
+using Snake.Presentation.LevelGenerator;
+using Snake.Presentation.Scenes;
 using Snake.Presentation.Services;
 
-ISnakeGameObjectFactory snakeGameObjectFactory = new SnakeGameObjectFactory();
-ISnakeGameManagerFactory gameManagerFactory = new SnakeGameManagerFactory();
-ISnakeGameService gameService = new SnakeGameService(gameManagerFactory, snakeGameObjectFactory);
-IGameSnakeRenderService render = new ConsoleGameSnakeRenderService();
+IServiceCollection services = new ServiceCollection();
+services.RegisterFactories();
+services.RegisterApplication();
+services.AddSingleton<IGameSnakeRenderService, ConsoleGameSnakeRenderService>();
+services.AddSingleton<IController, SnakeGameController>();
+services.AddSingleton<SnakeGame>();
+services.AddSingleton<SnakeMenu>();
+services.AddSingleton<ILevelGenerator, DefaultLevelGenerator>();
+services.AddSingleton(typeof(InputHandler), (s) =>
+{
+    var ih = new InputHandler();
+    ih.StartHandleConsoleInput();
+    return ih;
+});
 
-var level = new Level(new(20, 20), new List<PosXY>() { });
+var provider = services.BuildServiceProvider();
+var snakeMenu = provider.GetService<SnakeMenu>();
+var snakeGame = provider.GetService<SnakeGame>();
 
-var startPos = new PosXY(5, 5);
-var gameEngine = new GameEngine(gameService, render);
-gameEngine.Start(startPos, level);
+var controller = provider.GetService<IController>();
+controller.AddSceneToController(nameof(snakeMenu), snakeMenu);
+controller.AddSceneToController(nameof(snakeGame), snakeGame);
+
+snakeGame.Select();
+await controller.Start();
