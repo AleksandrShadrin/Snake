9036153104d7404b73c797d55f1a99f0d3e8698f
Alex <ssaasshha11@yandex.ru>
2022-09-22 20:03:51 +0400

    Add exit scene

---
+++ src/Snake.Presentation/Scenes/Exit.cs
@@ -0,0 +1,35 @@
+﻿using Snake.Presentation.Base;
+
+namespace Snake.Presentation.Scenes
+{
+    public class Exit : BaseScene
+    {
+        public Action ExitFromApp { get; set; }
+
+        private readonly InputHandler _inputHandler;
+        public Exit(InputHandler inputHandler)
+        {
+            _inputHandler = inputHandler;
+        }
+        public override void DoOnKeyPressed()
+        {
+            if(_inputHandler.ConsoleKey.HasValue)
+            {
+                ExitFromApp?.Invoke();
+                Console.Clear();
+            }
+        }
+
+        public override void Render()
+        {
+            Console.WriteLine("Press any key.");
+        }
+
+        public override void StartScene()
+        {
+            _inputHandler.ClearConsoleKeyInfo();
+            Console.Clear();
+            Render();
+        }
+    }
+}
