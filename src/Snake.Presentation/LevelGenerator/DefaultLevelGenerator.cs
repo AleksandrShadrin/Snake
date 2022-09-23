518505bc5c06970cd86097605c33e3abd17171cc
Alex <ssaasshha11@yandex.ru>
2022-09-23 20:46:12 +0400

    improve logic

--- src/Snake.Presentation/LevelGenerator/DefaultLevelGenerator.cs
+++ src/Snake.Presentation/LevelGenerator/DefaultLevelGenerator.cs
@@ -5,6 +5,6 @@ namespace Snake.Presentation.LevelGenerator
     internal class DefaultLevelGenerator : ILevelGenerator
     {
         public (PosXY, Level) GenerateLevel()
-        => (new PosXY(0, 0), new Level(new PosXY(30, 30), Enumerable.Empty<PosXY>()));
+        => (new PosXY(0, 0), new Level(new PosXY(Console.WindowWidth - 1, Console.WindowHeight - 1), Enumerable.Empty<PosXY>()));
     }
 }
