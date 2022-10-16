b7c9cecd4645d19c6bbe68b87040c110a93802a7
Alex <ssaasshha11@yandex.ru>
2022-10-16 12:28:33 +0400

    Add comments

--- src/Snake.Application/Adapters/IGameSaveLoader.cs
+++ src/Snake.Application/Adapters/IGameSaveLoader.cs
@@ -2,8 +2,20 @@
 {
     public interface IGameSaveLoader
     {
+        /// <summary>
+        /// Возвращает файлы сохранений
+        /// </summary>
+        /// <returns> Возвращает IEnumerable&lt;string&gt; - коллекцию имен файлов</returns>
         IEnumerable<string> GetSaveFiles();
+
+        /// <summary>
+        /// Сохраняет игру
+        /// </summary>
         void SaveGame();
+
+        /// <summary>
+        /// Загружает игру
+        /// </summary>
         void LoadGame(string fileName);
     }
 }
--- src/Snake.Application/Adapters/ISnakeGameService.cs
+++ src/Snake.Application/Adapters/ISnakeGameService.cs
@@ -6,18 +6,78 @@ namespace Snake.Application.Adapters
 {
     public interface ISnakeGameService
     {
+        /// <summary>
+        /// Возвращает текущее направление змейки
+        /// </summary>
+        /// <returns>Direction - направление змейки</returns>
         Direction CurrentDirection();
+
+        /// <summary>
+        /// Создаёт обычную игру, с заданными начальным положением и уровнем
+        /// </summary>
+        /// <param name="pos">начальное положение змейки</param>
+        /// <param name="level">уровень (стены, награды)</param>
         void CreateGame(PosXY pos, Level level);
+
+        /// <summary>Проверка окончания игры</summary>
+        /// <returns>true, если игра окончена</returns>
         bool GameIsOver();
+
+        /// <summary>Возвращает текущее количество очков</summary>
+        /// <returns>положительное число</returns>
         uint GetScore();
+
+        /// <summary>
+        /// Добавляет награду
+        /// </summary>
+        /// <param name="reward">RewardObject, который будет добавлен в игру</param>
         void AddRewardObject(RewardObject reward);
+
+        /// <summary>
+        /// Удаляет награду
+        /// </summary>
+        /// <param name="reward">RewardObject, который будет удален из игры</param>
         void RemoveRewardObject(RewardObject reward);
+
+        /// <summary>
+        /// Перемещает змейку на один шаг
+        /// </summary>
         void MoveSnake();
+
+        /// <summary>
+        /// Изменяет направление змейки
+        /// </summary>
+        /// <param name="direction">Direction, направление на которое пытаются поменять текущее значение</param>
         void ChangeMoveDirection(Direction direction);
+
+        /// <summary>
+        /// Загрузка игры
+        /// </summary>
+        /// <param name="data">SnakeGameData, объект хранящий состояние игры</param>
         void LoadGame(SnakeGameData data);
+
+        /// <summary>
+        /// Вовзращает текущий Level
+        /// </summary>
+        /// <returns>Возвращает объект Level</returns>
         Level GetLevel();
+
+        /// <summary>
+        /// Вовзращает текущий Level
+        /// </summary>
+        /// <returns>Возвращает объект Level</returns>
         IEnumerable<PosXY> GetWalls();
+
+        /// <summary>
+        /// Вовзращает награды
+        /// </summary>
+        /// <returns>Возвращает коллекцию наград</returns>
         IReadOnlyList<RewardObject> GetRewards();
+
+        /// <summary>
+        /// Вовзращает тело змейку
+        /// </summary>
+        /// <returns>Возвращает объект SnakeBodyObject</returns>
         SnakeBodyObject GetSnakeBody();
     }
 }
